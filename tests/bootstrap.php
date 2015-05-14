0eaaf3e1d1d0506ea33998a59cf049bfcedb32dc
Tomas Votruba <tomas.vot@gmail.com>
2015-05-15 01:03:30 +0200

    tests: remove temp fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,5 +7,5 @@ define('TEMP_DIR', $tempDir);
 
 @mkdir($tempDir, 0777, TRUE);
 register_shutdown_function(function () {
-//	Nette\Utils\FileSystem::delete(__DIR__ . '/temp');
+	Nette\Utils\FileSystem::delete(__DIR__ . '/temp');
 });
